59fe24d9651c3d8c62b3af8811629cc96fbe7b68
Bikash Bishwokarma <bikash.bishwokarma@gmail.com>
2017-06-21 00:59:06 -0500

    Improved default rule

--- AspNetMvc.Authorization.PermissionBased/Rules/AuthorizeWhenUserHasPermission.cs
+++ AspNetMvc.Authorization.PermissionBased/Rules/AuthorizeWhenUserHasPermission.cs
@@ -3,18 +3,19 @@
 namespace AspNetMvc.Authorization.PermissionBased.Rules
 {
     /// <summary>
-    /// When the user has permission(s), then this rule returns true.
+    /// When the signed in user does not have the specified permission(s), then 
+    /// this rule will return false, signigying the user does not have permission.
     /// </summary>
     public class AuthorizeWhenUserHasPermission : AuthorizationRule
     {
-        public bool Authorize(string userId, string resourceId)
-        {
-            return true;
-        }
-
         public bool For(ICollection<string> permissions, string resourceType)
         {
-            return permissions.Count > 0;
+            return permissions.Count == 0;
+        }
+
+        public bool Authorize(string userId, string resourceId)
+        {
+            return false;
         }
     }
 }
